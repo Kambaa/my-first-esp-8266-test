774cf1153fdea822b4cabc9796a844051d36e80d
Yusuf Gündüz <yusuf.gunduz@epias.com.tr>
2018-07-30 11:46:38 +0300

    serial read example written.

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,18 +5,38 @@
 #include "Main.h"
 #include "Arduino.h"
 #include "ESP8266WiFi.h"
-int ESP_LED_NO=2;
-void setup(){
-Serial.begin(9600);
+
+int ESP_LED_NO = 2;
+bool enableBlinking = false;
+int blinkingInterval = 500;
+
+void setup() {
+    Serial.begin(9600);
 // WiFi.begin("ssid","password");
-pinMode(ESP_LED_NO,OUTPUT);
+    pinMode(ESP_LED_NO, OUTPUT);
 }
 
-void loop(){
-    Serial.println("Hello Wold!");
-    delay(500);
-    digitalWrite(ESP_LED_NO,HIGH);
-    delay(500);
-    digitalWrite(ESP_LED_NO,LOW);
-    // Serial.read();
+void loop() {
+    if (enableBlinking) {
+        delay(blinkingInterval);
+        digitalWrite(ESP_LED_NO, HIGH);
+        delay(blinkingInterval);
+        digitalWrite(ESP_LED_NO, LOW);
+    } else {
+        while (Serial.available()) {
+            Serial.println("Enable led blinking? (y/n): ");
+            String isEnabled = Serial.readString();
+            if (isEnabled == "y") {
+                enableBlinking = true;
+                Serial.println("Enter led interval: ");
+                String temp = Serial.readString();
+                if (temp.toInt() > 0) {
+                    blinkingInterval = temp.toInt();
+                }
+            } else {
+                enableBlinking = false;
+            }
+            break;
+        }
+    }
 }
