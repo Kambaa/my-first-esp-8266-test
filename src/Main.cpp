ab9ad8678a6717dbe39b9f95616f5c895b4928a3
Yusuf Gündüz <yusuf.gunduz@epias.com.tr>
2018-07-31 14:51:29 +0300

    first wifi connection script written.

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,42 +6,47 @@
 #include "Arduino.h"
 #include "ESP8266WiFi.h"
 
-int ESP_LED_NO = 2;
-bool enableBlinking = false;
-int blinkingInterval = 500;
+const char *ssid = "yg";
+const char *password = "yg1234565";
 
 void setup() {
     Serial.begin(9600);
-// WiFi.begin("ssid","password");
-    pinMode(ESP_LED_NO, OUTPUT);
+//    Serial.begin(115200);
+    delay(15000);
+    // attempt to connect to Wifi network:
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+       would try to act as both a client and an access-point and could cause
+       network-issues with your other WiFi-devices on your WiFi-network. */
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
 }
 
 void loop() {
-    if (enableBlinking) {
-        delay(blinkingInterval);
-        digitalWrite(ESP_LED_NO, HIGH);
-        delay(blinkingInterval);
-        digitalWrite(ESP_LED_NO, LOW);
-    } else {
-        while (Serial.available()) {
-            Serial.println("Enable led blinking? (y/n): ");
-            String isEnabled = Serial.readString();
-            Serial.println("You have entered: " + isEnabled);
-            if (isEnabled == "y") {
-                enableBlinking = true;
-//                  Serial.println("Enter led interval: ");
-//                String temp = Serial.readStringUntil('!');
-//                Serial.println("You have entered: " + temp);
-//                if (temp.toInt() > 0) {
-//                    blinkingInterval = temp.toInt();
-//                }
-                Serial.println("Configuration saved. Now watch the led blinking!");
-            } else if (isEnabled == "n") {
-                enableBlinking = false;
-            } else {
-                continue;
-            }
+    Serial.println("You are in the loop method. Waiting for 3 sec...");
+    delay(3000);
+    String status;
+    switch (WiFi.status()) {
+        case WL_CONNECTED :
+            status = "Connected";
             break;
-        }
+        default:
+            status = "NOT CONNECTED";
+            break;
     }
+
+    Serial.println("Wifi status: " + status);
+    Serial.println("Local IP: " + WiFi.localIP().toString());
+    Serial.println("BSSIDStr: " + WiFi.BSSIDstr());
+    Serial.println("Wifi hostname: " + WiFi.hostname());
+    Serial.println("End of loop method.");
 }
